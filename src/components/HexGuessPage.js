261f4c69c25c03dd40dddabf168b9ef9b288c39a
kyle nicolaysen <kylenicolaysen@gmail.com>
2023-01-15 22:06:51 -0800

    fix title

--- src/components/HexGuessPage.js
+++ src/components/HexGuessPage.js
@@ -3,13 +3,13 @@ import React from 'react'
 const getRandomColor = () => "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);})
 const colorList = [getRandomColor(), getRandomColor(), getRandomColor(), getRandomColor()]
 const winner = Math.floor(Math.random() * 4) 
-let win1 = "none"
+let score = 0
 
 class HexGuessPage extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      gameState: 'not answered'
+      gameState: 'not answered',
     }
   }
 
@@ -18,11 +18,14 @@ class HexGuessPage extends React.Component {
     const result = (winner === answer) ? 'correct' : 'incorrect'
     const winBox = document.getElementById(winner.toString())
     winBox.style.border = `.75rem solid #75D968`
+    this.setState({
+      gameState: 'answered'
+    })
     if (result === 'incorrect') {
       const answerBox = document.getElementById(answer.toString())
-      console.log(answerBox)
       answerBox.style.border = '.75rem solid #FF3B1C'
     }
+    console.log(result)
     this.setState({
       gameState: 'answered'
     })
@@ -40,6 +43,7 @@ class HexGuessPage extends React.Component {
           <div className="item question-box">
             <p>What color is {colorList[winner]}?</p>
             {this.state.gameState === 'answered' ? <button onClick={this.reloadPage}>Try Again</button> : <div></div>}
+            <p>Score: {score}</p>
           </div>
           <div
             className="item"
